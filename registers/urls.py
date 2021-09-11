b22d3c31153287eded9e29b030ed1a59be2c79e0
Ayikanying <Ayikanying@users.noreply.github.com>
2021-09-11 16:38:24 +0300

    fixing FBV to CBV

--- registers/urls.py
+++ registers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CreateUser, name='register'),
+    path('', views.CreateUser.as_view(), name='register'),
     path('login/', views.login_request, name ='login_request'),
     path('ajax/validate-username/', views.ValidateUsername.as_view(), name="validate-username")
 ]
--- registers/views.py
+++ registers/views.py
@@ -5,18 +5,11 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from django.views.generic.base import View
-
+from django.views.generic.edit import CreateView
 # Create your views here.
-def CreateUser(request):
-    form = UserRegisterView()
-    if request.method == "POST":
-        form = UserRegisterView(request.POST)
-        if form.is_valid:
-            form.save()
-        
-
-    context = {"form": form}
-    return render(request, 'registration/register.html', context)
+class CreateUser(CreateView):
+    form_class = UserRegisterView
+    template_name = 'registration/register.html'
 
 def login_request(request):
     form = LoginForm()
